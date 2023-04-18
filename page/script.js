57ddb305a40f32057ee4243d8fd7207564855e6f
Muhammed Afsal <130966619+AfZal32@users.noreply.github.com>
2023-04-18 07:59:57 +0100

    Add files via upload

---
+++ page/script.js
@@ -0,0 +1,38 @@
+function openNewWindow(url) {
+    window.open(url, '_blank');
+  }
+
+  function phones() {
+    openNewWindow('phones.html');
+  }
+
+  function cases() {
+    openNewWindow('https://www.example.com/cases');
+  }
+
+  function hf() {
+    openNewWindow('https://www.example.com/headphones');
+  }
+
+  function charger() {
+    openNewWindow('https://www.example.com/chargers');
+  }
+  
+function shop(){
+    openNewWindow('products.html')
+}
+
+function home(){
+    openNewWindow('page.html')
+}
+function about(){
+    openNewWindow('contact.html')
+}
+function order(){
+  openNewWindow('orders.html')
+}
+
+
+function onSubmit() {
+  alert("Thank you for your order! We will contact you soon.");
+}
